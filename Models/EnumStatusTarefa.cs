0a10e1674bc0d47ab243041c0be0b3e18c8f46ed
Vitor Esbrolio Lima <74936968+VitorLima1@users.noreply.github.com>
2024-06-25 18:29:57 -0300

    Update EnumStatusTarefa.cs

--- Models/EnumStatusTarefa.cs
+++ Models/EnumStatusTarefa.cs
@@ -1,8 +1,18 @@
 namespace TrilhaApiDesafio.Models
 {
+    public class Tarefa
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+        public EnumStatusTarefa Status { get; set; }
+    }
+
     public enum EnumStatusTarefa
     {
         Pendente,
-        Finalizado
+        EmProgresso,
+        Concluido
     }
 }
